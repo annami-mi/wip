ddfa271b83acb4a2b01c67a89e9fbeced3e6b2bb
Михайлова Анна Дмитриевна <a.mikhailova@sibtrans.ru>
2024-09-11 16:11:18 +0700

    test router

--- src/app/router/index.ts
+++ src/app/router/index.ts
@@ -2,12 +2,10 @@ import {createRouter, createWebHashHistory} from 'vue-router'
 import { routes } from './routes'
 
 export const router = createRouter({
-    history: createWebHashHistory('/wip'),
+    history: createWebHashHistory(),
     routes,
 
     scrollBehavior(_, __, savedPosition) {
         return savedPosition || { top: 0 , behavior: 'smooth'}
     },
 })
-
-export default router
