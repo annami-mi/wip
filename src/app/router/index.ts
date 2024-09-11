0d18b85917281e203c3218bd0d6604b1b32ab684
Михайлова Анна Дмитриевна <a.mikhailova@sibtrans.ru>
2024-09-11 15:45:14 +0700

    test router

--- src/app/router/index.ts
+++ src/app/router/index.ts
@@ -2,7 +2,8 @@ import { createRouter, createWebHistory } from 'vue-router'
 import { routes } from './routes'
 
 export const router = createRouter({
-    history: createWebHistory('/wip'),
+    // history: createWebHistory('/wip'),
+    history: createWebHistory(),
     routes,
 
     scrollBehavior(_, __, savedPosition) {
